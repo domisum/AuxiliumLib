615a4cb423d8a976da728ee3a1a9c8113654ef22
Dominik <dominik.weissenseel@gmx.de>
2016-06-23 23:00:44 +0200

    Added image util and refactored

--- src/main/java/de/domisum/auxiliumapi/util/FileUtil.java
+++ src/main/java/de/domisum/auxiliumapi/util/FileUtil.java
@@ -1,4 +1,4 @@
-package de.domisum.auxiliumapi.util.java;
+package de.domisum.auxiliumapi.util;
 
 import java.io.File;
 import java.io.FileWriter;
--- src/main/java/de/domisum/auxiliumapi/util/TextUtil.java
+++ src/main/java/de/domisum/auxiliumapi/util/TextUtil.java
@@ -1,4 +1,4 @@
-package de.domisum.auxiliumapi.util.java;
+package de.domisum.auxiliumapi.util;
 
 import java.util.ArrayList;
 import java.util.List;
