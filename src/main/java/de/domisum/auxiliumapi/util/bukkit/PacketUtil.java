d59d47d001f3d9aa554201421cc78c43059aa22c
Dominik <dominik.weissenseel@gmx.de>
2016-06-23 15:22:00 +0200

    Added craftbukkit dependency and packet methods

---
+++ src/main/java/de/domisum/auxiliumapi/util/bukkit/PacketUtil.java
@@ -0,0 +1,36 @@
+package de.domisum.auxiliumapi.util.bukkit;
+
+import java.util.Collection;
+
+import org.bukkit.craftbukkit.v1_9_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import net.minecraft.server.v1_9_R1.Packet;
+
+public class PacketUtil
+{
+
+	// VALUE CONVERSION
+	public static int toPacketDistance(double d)
+	{
+		return (int) Math.floor(d * 32) * 128;
+	}
+
+	public static byte toPacketAngle(float f)
+	{
+		return (byte) ((int) ((f * 256.0f) / 360.0f));
+	}
+
+
+	// SENDING
+	public static void sendPacket(Packet<?> packet, Player... players)
+	{
+		for(Player p : players)
+			((CraftPlayer) p).getHandle().playerConnection.sendPacket(packet);
+	}
+
+	public static void sendPacket(Packet<?> packet, Collection<? extends Player> players)
+	{
+		sendPacket(packet, players.toArray(new Player[players.size()]));
+	}
+}
