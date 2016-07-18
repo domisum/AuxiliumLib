dc733e9a51302a2a27b18f112094a26b8af34265
Dominik <dominik.weissenseel@gmx.de>
2016-07-18 18:18:29 +0200

    Added locking system

--- src/main/java/de/domisum/auxiliumapi/monitor/PlayerVelocityMonitorManager.java
+++ src/main/java/de/domisum/auxiliumapi/monitor/PlayerVelocityMonitorManager.java
@@ -1,7 +1,10 @@
 package de.domisum.auxiliumapi.monitor;
 
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import org.bukkit.Bukkit;
@@ -20,6 +23,7 @@ public class PlayerVelocityMonitorManager implements Listener
 
 	// REFERENCES
 	protected Map<Player, PlayerVelocityMonitor> playerVelocityMonitors = new HashMap<Player, PlayerVelocityMonitor>();
+	protected Map<Player, List<WeakReference<Object>>> playerVelocityMonitorLocks = new HashMap<Player, List<WeakReference<Object>>>();
 
 	// STATUS
 	protected BukkitTask updateTask;
@@ -40,17 +44,51 @@ public class PlayerVelocityMonitorManager implements Listener
 	}
 
 
-	public static PlayerVelocityMonitor getFor(Player player)
+	// -------
+	// REGISTRATION
+	// -------
+	public static PlayerVelocityMonitor getFor(Player player, Object lock)
 	{
 		PlayerVelocityMonitorManager pvmm = AuxiliumAPI.getPlayerVelocityMonitorManager();
 		if(pvmm.playerVelocityMonitors.containsKey(player))
+		{
+			pvmm.addLock(player, lock);
 			return pvmm.playerVelocityMonitors.get(player);
+		}
 
 		PlayerVelocityMonitor pvm = new PlayerVelocityMonitor(player);
 		pvmm.playerVelocityMonitors.put(player, pvm);
+		pvmm.addLock(player, lock);
 		pvmm.startUpdateTask();
 
 		return pvm;
+	}
+
+	protected void addLock(Player player, Object lock)
+	{
+		List<WeakReference<Object>> locks = this.playerVelocityMonitorLocks.get(player);
+		if(locks == null)
+		{
+			locks = new ArrayList<WeakReference<Object>>();
+			this.playerVelocityMonitorLocks.put(player, locks);
+		}
+
+		WeakReference<Object> wr = new WeakReference<Object>(lock);
+		locks.add(wr);
+	}
+
+	protected boolean isMonitorUnlocked(Player player)
+	{
+		List<WeakReference<Object>> locks = this.playerVelocityMonitorLocks.get(player);
+		if(locks == null)
+			return true;
+
+		Iterator<WeakReference<Object>> iterator = locks.iterator();
+		while(iterator.hasNext())
+			if(iterator.next().get() == null)
+				iterator.remove();
+
+		return locks.size() == 0;
 	}
 
 
@@ -62,27 +100,7 @@ public class PlayerVelocityMonitorManager implements Listener
 		if(this.updateTask != null)
 			return;
 
-		Runnable run = () ->
-		{
-			Iterator<PlayerVelocityMonitor> iterator = this.playerVelocityMonitors.values().iterator();
-			while(iterator.hasNext())
-			{
-				PlayerVelocityMonitor pvm = iterator.next();
-				Player player = pvm.getPlayer();
-
-				if(!player.isOnline() || pvm.isTerminated())
-					iterator.remove();
-				else
-					pvm.tick();
-			}
-
-			System.out.println(this.playerVelocityMonitors.size());
-
-			if(this.playerVelocityMonitors.size() == 0)
-				stopUpdateTask();
-		};
-
-		this.updateTask = Bukkit.getScheduler().runTaskTimer(AuxiliumAPI.getPlugin(), run, 1, 1);
+		this.updateTask = Bukkit.getScheduler().runTaskTimer(AuxiliumAPI.getPlugin(), () -> update(), 1, 1);
 	}
 
 	protected void stopUpdateTask()
@@ -92,6 +110,29 @@ public class PlayerVelocityMonitorManager implements Listener
 
 		this.updateTask.cancel();
 		this.updateTask = null;
+	}
+
+	protected void update()
+	{
+		Iterator<PlayerVelocityMonitor> iterator = this.playerVelocityMonitors.values().iterator();
+		while(iterator.hasNext())
+		{
+			PlayerVelocityMonitor pvm = iterator.next();
+			Player player = pvm.getPlayer();
+
+			if(!player.isOnline() || pvm.isTerminated() || isMonitorUnlocked(player))
+			{
+				iterator.remove();
+				this.playerVelocityMonitorLocks.remove(player);
+			}
+			else
+				pvm.tick();
+		}
+
+		System.out.println(this.playerVelocityMonitors.size());
+
+		if(this.playerVelocityMonitors.size() == 0)
+			stopUpdateTask();
 	}
 
 
