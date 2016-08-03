c9431d301c0cc18d0c4125ea6478449196743c0b
Dominik <dominik.weissenseel@gmx.de>
2016-08-04 01:00:15 +0200

    Added vectorutil

--- src/main/java/de/domisum/auxiliumapi/util/math/MathUtil.java
+++ src/main/java/de/domisum/auxiliumapi/util/math/MathUtil.java
@@ -1,8 +1,5 @@
 package de.domisum.auxiliumapi.util.math;
 
-import de.domisum.auxiliumapi.data.container.math.Vector2D;
-import de.domisum.auxiliumapi.data.container.math.Vector3D;
-
 public class MathUtil
 {
 
@@ -37,77 +34,6 @@ public class MathUtil
 			delta = (2 * Math.PI) - delta;
 
 		return delta <= maxD;
-	}
-
-
-	// VECTOR
-	public static Vector2D getDirectionVector(double yaw)
-	{
-		double dX = -Math.sin(yaw);
-		double dZ = Math.cos(yaw);
-
-		return new Vector2D(dX, dZ);
-	}
-
-	public static Vector3D getCenter(Vector3D vector)
-	{
-		return new Vector3D(Math.floor(vector.x) + .5, Math.floor(vector.y) + .5, Math.floor(vector.z) + .5);
-	}
-
-
-	public static Vector3D getPointOnLineClosestToPoint(Vector3D l1, Vector3D l2, Vector3D p)
-	{
-		Vector3D v = l2.subtract(l1);
-		Vector3D w = p.subtract(l1);
-
-		double wvProduct = w.dotProduct(v);
-		double vvProduct = v.dotProduct(v);
-
-		double productQuot = wvProduct / vvProduct;
-		Vector3D pointOnLine = l1.add(v.multiply(productQuot));
-		return pointOnLine;
-	}
-
-
-	public static double getDistanceFromLineToPoint(Vector2D l1, Vector2D l2, Vector2D p)
-	{
-		// is this even right? may be just for line segment instead of infinite line
-
-		double normalLength = Math.sqrt(((l2.x - l1.x) * (l2.x - l1.x)) + ((l2.y - l1.y) * (l2.y - l1.y)));
-		return Math.abs(((p.x - l1.x) * (l2.y - l1.y)) - ((p.y - l1.y) * (l2.x - l1.x))) / normalLength;
-	}
-
-	public static double getDistanceFromLineToPoint(Vector3D l1, Vector3D l2, Vector3D p)
-	{
-		// http://mathworld.wolfram.com/Point-LineDistance3-Dimensional.html
-
-		Vector3D oneToTwo = l2.subtract(l1);
-		Vector3D pToOne = l1.subtract(p);
-
-		Vector3D crossProduct = oneToTwo.crossProduct(pToOne);
-
-		double distance = crossProduct.length() / oneToTwo.length();
-		return distance;
-	}
-
-	public static double getDistanceFromSegmentToPoint(Vector3D l1, Vector3D l2, Vector3D p)
-	{
-		// http://geomalgorithms.com/a02-_lines.html
-
-		Vector3D v = l2.subtract(l1);
-		Vector3D w = p.subtract(l1);
-
-		double wvProduct = w.dotProduct(v);
-		double vvProduct = v.dotProduct(v);
-		if(wvProduct <= 0)
-			return p.distanceTo(l1);
-		if(v.dotProduct(v) <= wvProduct)
-			return p.distanceTo(l2);
-
-		double productQuot = wvProduct / vvProduct;
-		Vector3D pointOnSegment = l1.add(v.multiply(productQuot));
-
-		return p.distanceTo(pointOnSegment);
 	}
 
 
