84c1322c300fa6706e5aa1c69a8ef2a65eada8c3
domisum <dominik.weissenseel@gmx.de>
2016-10-10 10:58:18 +0200

    Added javadoc comments to BlockUtil

--- src/main/java/de/domisum/lib/auxilium/util/bukkit/BlockUtil.java
+++ src/main/java/de/domisum/lib/auxilium/util/bukkit/BlockUtil.java
@@ -8,15 +8,39 @@ import org.bukkit.block.Block;
 public class BlockUtil
 {
 
-	// -------
+	/*
 	// SETTING
-	// -------
+	*/
+
+	/**
+	 * Sets the material and the subId of the block from the argument
+	 * to the material and the subId from the arguments.
+	 * <p>
+	 * This method doesn't trigger a physics update.
+	 *
+	 * @param block    the block
+	 * @param material the material
+	 * @param data     the subId
+	 * @see #setMaterialAndData(Block, Material, byte, boolean)
+	 */
 	@APIUsage
 	public static void setMaterialAndData(Block block, Material material, byte data)
 	{
 		setMaterialAndData(block, material, data, false);
 	}
 
+	/**
+	 * Sets the material and the subId of the block from the argument
+	 * to the material and the subId from the arguments.
+	 * <p>
+	 * The parameter {@code physics} determines whether a physics update should be
+	 * triggered on the block after the changes were made to it.
+	 *
+	 * @param block    the block
+	 * @param material the material
+	 * @param data     the subId
+	 * @param physics  whether a physics update should be triggered
+	 */
 	@SuppressWarnings("deprecation")
 	@APIUsage
 	public static void setMaterialAndData(Block block, Material material, byte data, boolean physics)
