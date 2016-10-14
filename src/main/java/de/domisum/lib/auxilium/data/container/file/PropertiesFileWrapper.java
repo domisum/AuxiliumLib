7d801a193acaaf7b097bf191419828490a0e4f74
Dominik <dominik.weissenseel@gmx.de>
2016-10-14 21:44:47 +0200

    Added saving and modification to properties file wrapper

--- src/main/java/de/domisum/lib/auxilium/data/container/file/PropertiesFileWrapper.java
+++ src/main/java/de/domisum/lib/auxilium/data/container/file/PropertiesFileWrapper.java
@@ -1,9 +1,13 @@
 package de.domisum.lib.auxilium.data.container.file;
 
 import de.domisum.lib.auxilium.util.java.annotations.APIUsage;
+import lombok.Getter;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Properties;
 
 @APIUsage
@@ -14,27 +18,49 @@ public class PropertiesFileWrapper
 	private String filePath;
 
 	// REFERENCES
-	private Properties properties;
+	@Getter private Properties properties;
 
 
 	/*
-	// CONSTRUCTOR
+	// INITIALIZATION
 	*/
 	@APIUsage
+
 	public PropertiesFileWrapper(String filePath)
 	{
 		this.filePath = filePath;
 
-		loadFile();
+		loadFromFile();
 	}
 
-	private void loadFile()
+
+	/*
+	// LOADING
+	*/
+	private void loadFromFile()
 	{
 		properties = new Properties();
 
 		try(InputStream inputStream = new FileInputStream(filePath))
 		{
 			properties.load(inputStream);
+		}
+		catch(java.io.IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+
+	/*
+	// SAVING
+	*/
+	@APIUsage
+	public void saveToFile(File file, String comment)
+	{
+		try(OutputStream outputStream = new FileOutputStream(file))
+		{
+			properties.store(outputStream, comment);
 		}
 		catch(java.io.IOException e)
 		{
@@ -53,6 +79,16 @@ public class PropertiesFileWrapper
 			throw new IllegalArgumentException("The properties file '"+filePath+"' does not contain the key '"+key+"'");
 
 		return properties.getProperty(key);
+	}
+
+
+	/*
+	// SETTERS
+	*/
+	@APIUsage
+	public void set(String key, String value)
+	{
+		properties.setProperty(key, value);
 	}
 
 }
