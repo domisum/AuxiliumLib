ef3151792804756c7c6682b1b03230f3c0212760
Dominik <dominik.weissenseel@gmx.de>
2017-11-13 15:20:43 +0100

    Cleaned up ProfilerStopWatch

--- src/main/java/de/domisum/lib/auxilium/util/java/ProfilerStopWatch.java
+++ src/main/java/de/domisum/lib/auxilium/util/java/ProfilerStopWatch.java
@@ -2,26 +2,43 @@ package de.domisum.lib.auxilium.util.java;
 
 import de.domisum.lib.auxilium.util.java.annotations.API;
 import de.domisum.lib.auxilium.util.math.MathUtil;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @API
+@RequiredArgsConstructor
 public class ProfilerStopWatch
 {
 
 	// PROPERTIES
-	private String name;
-	private long startNano;
-	private long endNano = -1;
+	private final String name;
+
+	// TEMP
+	@Getter private transient long startNano;
+	private transient long endNano = -1;
 
 
-	// INIT
-	@API public ProfilerStopWatch(String name)
-	{
-		this.name = name;
-	}
-
+	// OBJECT
 	@Override public String toString()
 	{
 		return "[name="+this.name+";active="+isActive()+";elapsedMs="+MathUtil.round(getElapsedMilli(), 3)+"]";
+	}
+
+
+	// GETTERS
+	@API public boolean isActive()
+	{
+		return this.endNano == -1;
+	}
+
+	@API public long getElapsedNano()
+	{
+		return (this.endNano == -1 ? System.nanoTime() : this.endNano)-this.startNano;
+	}
+
+	@API public double getElapsedMilli()
+	{
+		return getElapsedNano()/1000d/1000d;
 	}
 
 
@@ -37,28 +54,6 @@ public class ProfilerStopWatch
 			throw new IllegalStateException("The stopwatch has already been stopped");
 
 		this.endNano = System.nanoTime();
-	}
-
-
-	// GETTERS
-	@API public long getStartNano()
-	{
-		return this.startNano;
-	}
-
-	@API public boolean isActive()
-	{
-		return this.endNano == -1;
-	}
-
-	@API public long getElapsedNano()
-	{
-		return (this.endNano == -1 ? System.nanoTime() : this.endNano)-this.startNano;
-	}
-
-	@API public double getElapsedMilli()
-	{
-		return getElapsedNano()/1000d/1000d;
 	}
 
 }
