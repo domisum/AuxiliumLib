ca7c283638ad3c2c79a20beb21b8bee639e60b52
Dominik <dominik.weissenseel@gmx.de>
2017-11-27 22:48:21 +0100

    Added add methods to Coordinate2DInt

--- src/main/java/de/domisum/lib/auxilium/data/container/math/Coordinate2DInt.java
+++ src/main/java/de/domisum/lib/auxilium/data/container/math/Coordinate2DInt.java
@@ -15,4 +15,15 @@ public class Coordinate2DInt
 	public final int x;
 	public final int y;
 
+
+	public Coordinate2DInt add(Coordinate2DInt other)
+	{
+		return new Coordinate2DInt(x+other.x, y+other.y);
+	}
+
+	public Coordinate2DInt add(int dX, int dY)
+	{
+		return new Coordinate2DInt(x+dX, y+dY);
+	}
+
 }
