d3ee2ba87b7875a64558da37b237d7ee1093da7b
Dominik <dominik.weissenseel@gmx.de>
2017-12-05 20:31:56 +0100

    StringUtilTest

---
+++ src/test/java/de/domisum/lib/auxilium/util/StringUtilTest.java
@@ -0,0 +1,28 @@
+package de.domisum.lib.auxilium.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StringUtilTest
+{
+
+	// TEST COMMON PREFIX
+	@Test public void testPrefixNoCommon()
+	{
+		Assertions.assertEquals("", StringUtil.getCommonPrefix("", ""));
+		Assertions.assertEquals("", StringUtil.getCommonPrefix("b", "a"));
+		Assertions.assertEquals("", StringUtil.getCommonPrefix("bfischerd", "afischerd"));
+	}
+
+	@Test public void testPrefixCommon()
+	{
+		Assertions.assertEquals("common", StringUtil.getCommonPrefix("commonMeme", "commonXd"));
+
+		Assertions.assertEquals("a", StringUtil.getCommonPrefix("abcd", "asdf"));
+		Assertions.assertEquals("a", StringUtil.getCommonPrefix("a", "asdf"));
+		Assertions.assertEquals("a", StringUtil.getCommonPrefix("abcd", "a"));
+
+		Assertions.assertEquals("+-*/", StringUtil.getCommonPrefix("+-*/", "+-*/59"));
+	}
+
+}
