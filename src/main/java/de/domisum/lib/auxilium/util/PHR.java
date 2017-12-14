7530c054c821c21c96c3f029f8449eda8f1b28cd
Dominik <dominik.weissenseel@gmx.de>
2017-12-14 20:42:42 +0100

    Improved mismatched placeholder number error message

--- src/main/java/de/domisum/lib/auxilium/util/PHR.java
+++ src/main/java/de/domisum/lib/auxilium/util/PHR.java
@@ -7,20 +7,29 @@ public class PHR
 
 	public static String r(String text, Object... values)
 	{
-		String replacedText = text;
+		int numberOfFoundPlaceholders = 0;
 
+		String replacedText = text;
 		for(Object value : values)
 		{
 			if(!replacedText.contains("{}"))
-				throw new IllegalArgumentException("text doesn't contain enough placeholders");
+				throw new IllegalArgumentException(
+						"text doesn't contain enough placeholders ("+getPlaceHolderReport(numberOfFoundPlaceholders, values));
 
 			replacedText = replacedText.replaceFirst("\\{}", Objects.toString(value));
+			numberOfFoundPlaceholders++;
 		}
 
 		if(replacedText.contains("{}"))
-			throw new IllegalArgumentException("text contains too many placeholders");
+			throw new IllegalArgumentException(
+					"text contains too many placeholders ("+getPlaceHolderReport(numberOfFoundPlaceholders, values)+")");
 
 		return replacedText;
+	}
+
+	private static String getPlaceHolderReport(int numberOfFoundPlaceholders, Object... values)
+	{
+		return "given values: "+values.length+", found placeholders: "+numberOfFoundPlaceholders;
 	}
 
 }
