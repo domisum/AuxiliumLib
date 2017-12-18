a22d66bdffeb4ae804110bff5342d5ea2355cb91
Dominik <dominik.weissenseel@gmx.de>
2017-12-18 15:48:06 +0100

    Fixed old javadoc comment

--- src/main/java/de/domisum/lib/auxilium/contracts/storage/KeySource.java
+++ src/main/java/de/domisum/lib/auxilium/contracts/storage/KeySource.java
@@ -9,7 +9,7 @@ public interface KeySource<T, K>
 	 * Fetches the item associated with the given key.
 	 *
 	 * @param key the key of the object to retrieve from storage
-	 * @return the T associated with the key, or null if none found
+	 * @return the T associated with the key
 	 */
 	@API T fetch(K key);
 
