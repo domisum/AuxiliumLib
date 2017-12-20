8eb920a18a379e972550fd5c190b9d4c3b20687c
Dominik <dominik.weissenseel@gmx.de>
2017-12-20 12:05:52 +0100

    Added MultiGenerator contract

---
+++ src/main/java/de/domisum/lib/auxilium/contracts/generator/MultiGenerator.java
@@ -0,0 +1,11 @@
+package de.domisum.lib.auxilium.contracts.generator;
+
+import de.domisum.lib.auxilium.util.java.annotations.API;
+
+import java.util.List;
+
+@API
+public interface MultiGenerator<I, O> extends Generator<I, List<O>>
+{
+
+}
