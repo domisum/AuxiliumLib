7511f2e83cf0a39a115d2256ffaa4b51228e6dcf
Dominik <dominik.weissenseel@gmx.de>
2017-12-29 17:24:03 +0100

    Added FetchHttpToFile

---
+++ src/main/java/de/domisum/lib/auxilium/util/http/specific/HttpFetchToFile.java
@@ -0,0 +1,42 @@
+package de.domisum.lib.auxilium.util.http.specific;
+
+import de.domisum.lib.auxilium.data.container.AbstractURL;
+import de.domisum.lib.auxilium.util.http.HttpFetch;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+
+public class HttpFetchToFile extends HttpFetch<File>
+{
+
+	// SETTINGS
+	private final File downloadTo;
+
+
+	// INIT
+	public HttpFetchToFile(AbstractURL url, File downloadTo)
+	{
+		super(url);
+		this.downloadTo = downloadTo;
+	}
+
+
+	// FETCH
+	@Override protected Optional<File> fetch(InputStream inputStream)
+	{
+		try
+		{
+			FileUtils.copyInputStreamToFile(inputStream, downloadTo);
+			return Optional.of(downloadTo); // if this is reached, the download was successful
+		}
+		catch(IOException e)
+		{
+			getOnFail().handle(e);
+			return Optional.empty();
+		}
+	}
+
+}
