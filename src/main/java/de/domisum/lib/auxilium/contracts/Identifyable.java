bfdef46fb0af48fad02f5014970b1a3f4aa95742
Dominik <dominik.weissenseel@gmx.de>
2017-12-29 12:15:54 +0100

    Changed components check

--- src/main/java/de/domisum/lib/auxilium/contracts/Identifyable.java
+++ src/main/java/de/domisum/lib/auxilium/contracts/Identifyable.java
@@ -1,8 +1,31 @@
 package de.domisum.lib.auxilium.contracts;
 
-public interface Identifyable
+import de.domisum.lib.auxilium.contracts.storage.InMemoryProxyStorage.Keyable;
+import de.domisum.lib.auxilium.util.StringUtil;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public interface Identifyable extends Keyable<String>
 {
 
 	String getId();
+
+	@Override default String getKey()
+	{
+		return getId();
+	}
+
+
+	// UTIL
+	static String getIdList(Collection<? extends Identifyable> identifyables)
+	{
+		List<String> strings = new ArrayList<>();
+		for(Identifyable identifyable : identifyables)
+			strings.add(identifyable.getId());
+
+		return "["+StringUtil.listToString(strings, ", ")+"]";
+	}
 
 }
