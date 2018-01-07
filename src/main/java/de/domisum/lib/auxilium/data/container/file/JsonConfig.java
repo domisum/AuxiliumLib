93ce332cb2bd6e5fb4c9a87240a8b0d2a7d1b64f
Dominik <dominik.weissenseel@gmx.de>
2018-01-07 21:34:31 +0100

    Removed custom deserializers from JsonConfig

--- src/main/java/de/domisum/lib/auxilium/data/container/file/JsonConfig.java
+++ src/main/java/de/domisum/lib/auxilium/data/container/file/JsonConfig.java
@@ -1,23 +1,16 @@
 package de.domisum.lib.auxilium.data.container.file;
 
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializer;
 import de.domisum.lib.auxilium.util.FileUtil;
 import de.domisum.lib.auxilium.util.java.annotations.API;
 import de.domisum.lib.auxilium.util.java.annotations.InitByDeserialization;
-import de.domisum.lib.auxilium.util.java.exceptions.ShouldNeverHappenError;
+import de.domisum.lib.auxilium.util.json.GsonUtil;
+import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.Validate;
-import org.apache.commons.lang3.tuple.Pair;
 
 import java.io.File;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Collection;
 
 @API
-@InitByDeserialization
+@NoArgsConstructor
 public abstract class JsonConfig
 {
 
@@ -34,41 +27,13 @@ public abstract class JsonConfig
 
 	@API public static <T extends JsonConfig> T parse(String json, Class<T> tClass)
 	{
-		GsonBuilder builder = new GsonBuilder();
-		for(Pair<Class<Object>, JsonDeserializer<Object>> pair : getDeserializers(tClass))
-			builder.registerTypeAdapter(pair.getKey(), pair.getValue());
-
-
-		T jsonConfig = builder.create().fromJson(json, tClass);
+		T jsonConfig = GsonUtil.get().fromJson(json, tClass);
 
 		if(jsonConfig == null)
 			throw new IllegalArgumentException("empty string to parse to "+tClass.getName()+": '"+json+"'");
 		jsonConfig.validate();
 
 		return jsonConfig;
-	}
-
-	@SuppressWarnings({"JavaReflectionInvocation", "unchecked"})
-	private static <T, C> Collection<Pair<Class<T>, JsonDeserializer<T>>> getDeserializers(Class<C> clazz)
-	{
-		try
-		{
-			Constructor<?> constructor = clazz.getConstructor();
-			Object jsonConfig = constructor.newInstance();
-			Method method = clazz.getSuperclass().getDeclaredMethod("getDeserializers");
-			method.setAccessible(true);
-
-			return (Collection<Pair<Class<T>, JsonDeserializer<T>>>) method.invoke(jsonConfig);
-		}
-		catch(NoSuchMethodException|IllegalAccessException|InstantiationException|InvocationTargetException e)
-		{
-			throw new ShouldNeverHappenError(e);
-		}
-	}
-
-	@API protected Collection<Pair<Class<?>, JsonDeserializer<?>>> getDeserializers()
-	{
-		return new ArrayList<>();
 	}
 
 
