2c5a5d1def842b53ea39d355ab61b0f03d000ad6
Dominik <dominik.weissenseel@gmx.de>
2018-01-11 04:15:37 +0100

    Added serialized object reader

---
+++ src/main/java/de/domisum/lib/auxilium/http/response/readers/HttpSerializedObjectReader.java
@@ -0,0 +1,35 @@
+package de.domisum.lib.auxilium.http.response.readers;
+
+import de.domisum.lib.auxilium.contracts.serialization.ToStringSerializer;
+import de.domisum.lib.auxilium.http.response.HttpResponseBodyReader;
+import de.domisum.lib.auxilium.util.java.annotations.API;
+import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@API
+@RequiredArgsConstructor
+public class HttpSerializedObjectReader<T> implements HttpResponseBodyReader<T>
+{
+
+	private final HttpResponseBodyReader<String> stringReader;
+	private final ToStringSerializer<T> toStringSerializer;
+
+
+	// READ
+	@Override public T read(InputStream inputStream) throws IOException
+	{
+		String json = stringReader.read(inputStream);
+
+		try
+		{
+			return toStringSerializer.deserialize(json);
+		}
+		catch(RuntimeException e)
+		{
+			throw new IOException("Failed to deserialize object", e);
+		}
+	}
+
+}
