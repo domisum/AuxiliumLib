adc8785fdb6411ac869cdea3b9384553ef156ac1
Dominik <dominik.weissenseel@gmx.de>
2018-01-13 12:27:05 +0100

    Extended StrategySelector to also do multiselection

--- src/main/java/de/domisum/lib/auxilium/contracts/strategy/StrategySelector.java
+++ src/main/java/de/domisum/lib/auxilium/contracts/strategy/StrategySelector.java
@@ -2,6 +2,8 @@ package de.domisum.lib.auxilium.contracts.strategy;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,18 +12,36 @@ public class StrategySelector<T, StrategyT extends Strategy<T>>
 {
 
 	// ATTRIBUTES
-	private final StrategyT defaultStrategy;
 	private final List<StrategyT> strategies;
+	private final StrategyT fallbackStrategy;
+
+
+	// INIT
+	public StrategySelector(List<StrategyT> strategies)
+	{
+		fallbackStrategy = null;
+		this.strategies = Collections.unmodifiableList(strategies);
+	}
 
 
 	// SELECT
-	public Optional<StrategyT> selectFor(T strategizedObject)
+	public Optional<StrategyT> selectFirstApplicable(T strategizedObject)
 	{
 		for(StrategyT s : strategies)
 			if(s.doesApplyTo(strategizedObject))
 				return Optional.of(s);
 
-		return Optional.ofNullable(defaultStrategy);
+		return Optional.ofNullable(fallbackStrategy);
+	}
+
+	public List<StrategyT> selectAllApplicableFor(T strategizedObject)
+	{
+		List<StrategyT> applicable = new ArrayList<>();
+		for(StrategyT strategy : strategies)
+			if(strategy.doesApplyTo(strategizedObject))
+				applicable.add(strategy);
+
+		return applicable;
 	}
 
 }
