833ce4a06b4b81b6a6b518dde49c1c72b62fea22
Dominik <dominik.weissenseel@gmx.de>
2018-01-17 18:09:45 +0100

    Added in memory proxy source

--- src/main/java/de/domisum/lib/auxilium/contracts/source/specific/ImageSource.java
+++ src/main/java/de/domisum/lib/auxilium/contracts/source/specific/ImageSource.java
@@ -4,7 +4,7 @@ import de.domisum.lib.auxilium.contracts.source.Source;
 
 import java.awt.image.BufferedImage;
 
-public interface ImageSource<KeyT> extends Source<KeyT, BufferedImage>
+public interface ImageSource<KeyT> extends Source<KeyT, BufferedImage> // TODO remove this
 {
 
 }
