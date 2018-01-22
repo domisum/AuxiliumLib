4ea83de945c1d2a0ae44da3f90ba54992843c2da
Dominik <dominik.weissenseel@gmx.de>
2018-01-22 18:58:17 +0100

    Removed old roman numeral from AuxiliumLib

--- src/main/java/de/domisum/lib/auxilium/util/TextUtil.java
+++ src/main/java/de/domisum/lib/auxilium/util/TextUtil.java
@@ -14,44 +14,8 @@ public final class TextUtil
 
 	private static final int SECONDS_IN_MINUTE = 60;
 
+
 	// NUMBERS
-	@API public static String asRomanNumeral(int number)
-	{
-		if((number < 1) || (number > 3999))
-			throw new IllegalArgumentException(
-					"Only numbers from 1 to 3999 can be represented as roman numerals; number given: "+number);
-
-		int numberLeft = number;
-		StringBuilder roman = new StringBuilder();
-
-		int[] numbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-		String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-
-		for(int i = 0; i < numbers.length; i++)
-		{
-			// append until no longer applicable
-			boolean changed = true;
-			while(changed)
-			{
-				int newNumber = romanNumeralAppendIfApplicable(numberLeft, roman, numbers[i], symbols[i]);
-
-				changed = newNumber != numberLeft;
-				numberLeft = newNumber;
-			}
-		}
-
-		return roman.toString();
-	}
-
-	private static int romanNumeralAppendIfApplicable(int numberLeft, StringBuilder romanNumeral, int minimum, String toAppend)
-	{
-		if(numberLeft < minimum)
-			return numberLeft;
-
-		romanNumeral.append(toAppend);
-		return numberLeft-minimum;
-	}
-
 	@API public static String secondsToMinuteSeconds(int seconds)
 	{
 		boolean negative = seconds < 0;
