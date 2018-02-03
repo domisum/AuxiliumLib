c817d3cd1e043ca6f9caa9ed7b9fe9365612c751
Dominik <dominik.weissenseel@gmx.de>
2018-02-03 14:05:29 +0100

    Added option to get request response from GetUtil

--- src/main/java/de/domisum/lib/auxilium/mattp/util/MattpGetUtil.java
+++ src/main/java/de/domisum/lib/auxilium/mattp/util/MattpGetUtil.java
@@ -5,6 +5,7 @@ import de.domisum.lib.auxilium.mattp.MattpRequestEnvoy;
 import de.domisum.lib.auxilium.mattp.decorator.RetryingMattpRequestEnvoy;
 import de.domisum.lib.auxilium.mattp.request.MattpRequest;
 import de.domisum.lib.auxilium.mattp.response.MattpResponseBodyReader;
+import de.domisum.lib.auxilium.mattp.response.RequestResponse;
 import de.domisum.lib.auxilium.mattp.response.readers.MattpByteArrayReader;
 import de.domisum.lib.auxilium.mattp.response.readers.MattpImageReader;
 import de.domisum.lib.auxilium.mattp.response.readers.MattpStringReader;
@@ -23,28 +24,51 @@ public final class MattpGetUtil
 	// STRING
 	@API public static Optional<String> getString(AbstractURL url)
 	{
-		return get(url, new MattpStringReader());
+		return getStringResponse(url).getContent();
 	}
+
+	@API public static RequestResponse<String> getStringResponse(AbstractURL url)
+	{
+		return getResponse(url, new MattpStringReader());
+	}
+
 
 	// BYTE ARRAY
 	@API public static Optional<byte[]> getByteArray(AbstractURL url)
 	{
-		return get(url, new MattpByteArrayReader());
+		return getByteArrayResponse(url).getContent();
 	}
+
+	@API public static RequestResponse<byte[]> getByteArrayResponse(AbstractURL url)
+	{
+		return getResponse(url, new MattpByteArrayReader());
+	}
+
 
 	// IMAGE
 	@API public static Optional<BufferedImage> getImage(AbstractURL url)
 	{
-		return get(url, new MattpImageReader());
+		return getImageResponse(url).getContent();
 	}
 
+	@API public static RequestResponse<BufferedImage> getImageResponse(AbstractURL url)
+	{
+		return getResponse(url, new MattpImageReader());
+	}
+
+
 	// GENERIC
-	@API public static <T> Optional<T> get(AbstractURL url, MattpResponseBodyReader<T> responseBodyReader)
+	@API public static <T> RequestResponse<T> getResponse(AbstractURL url, MattpResponseBodyReader<T> responseBodyReader)
 	{
 		MattpRequest request = MattpRequest.get(url);
 		MattpRequestEnvoy<T> envoy = new RetryingMattpRequestEnvoy<>(request, responseBodyReader);
 
-		return envoy.send().getContent();
+		return envoy.send();
+	}
+
+	@API public static <T> Optional<T> get(AbstractURL url, MattpResponseBodyReader<T> responseBodyReader)
+	{
+		return getResponse(url, responseBodyReader).getContent();
 	}
 
 }
