90de0857bd25fcfb25efa1d74be6bd8d85ec8aab
Dominik <dominik.weissenseel@gmx.de>
2018-07-17 18:57:41 +0200

    Improved performance and added ignore usage to LazyKeyCache

--- src/main/java/de/domisum/lib/auxilium/data/container/LazyKeyCache.java
+++ src/main/java/de/domisum/lib/auxilium/data/container/LazyKeyCache.java
@@ -2,7 +2,7 @@ package de.domisum.lib.auxilium.data.container;
 
 import de.domisum.lib.auxilium.util.java.annotations.API;
 import de.domisum.lib.auxilium.util.time.DurationUtil;
-import lombok.AccessLevel;
+import de.domisum.lib.auxilium.util.time.PassiveTimer;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -13,15 +13,30 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @API
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public final class LazyKeyCache<KeyT, T>
 {
 
+	// SETTINGS
 	private final Duration expirationDuration;
+	private final boolean onlyExpireUnused;
+
+	// STATE
 	private final Map<KeyT, CacheEntry> cache = new ConcurrentHashMap<>();
+	private final PassiveTimer expireCheckDueTimer;
 
 
 	// INIT
+	@API public static <KeyT, T> LazyKeyCache<KeyT, T> neverExpires()
+	{
+		return expiresAfter(Duration.ofSeconds(Long.MAX_VALUE));
+	}
+
+	@API public static <KeyT, T> LazyKeyCache<KeyT, T> expiresAfter(Duration expirationDuration)
+	{
+		return new LazyKeyCache<>(expirationDuration, false);
+	}
+
+
 	@API public static <KeyT, T> LazyKeyCache<KeyT, T> neverExpiresUnused()
 	{
 		return unusedExpiresAfter(Duration.ofSeconds(Long.MAX_VALUE));
@@ -29,31 +44,56 @@ public final class LazyKeyCache<KeyT, T>
 
 	@API public static <KeyT, T> LazyKeyCache<KeyT, T> unusedExpiresAfter(Duration expirationDuration)
 	{
-		return new LazyKeyCache<>(expirationDuration);
+		return new LazyKeyCache<>(expirationDuration, true);
+	}
+
+
+	private LazyKeyCache(Duration expirationDuration, boolean onlyExpireUnused)
+	{
+		this.expirationDuration = expirationDuration;
+		this.onlyExpireUnused = onlyExpireUnused;
+
+		expireCheckDueTimer = PassiveTimer.fromDurationAndStart(expirationDuration);
 	}
 
 
 	// CACHE
 	public void put(KeyT key, T value)
 	{
-		removeExpiredEntries();
+		ifDueRemoveExpiredEntries();
 
-		CacheEntry newCacheEntry = new CacheEntry(value);
+		CacheEntry newCacheEntry = createCacheEntry(value);
 		cache.put(key, newCacheEntry);
+	}
+
+	private CacheEntry createCacheEntry(T value)
+	{
+		if(onlyExpireUnused)
+			return new CacheEntryRespectUsage(value);
+
+		return new CacheEntryIgnoreUsage(value);
 	}
 
 	public Optional<T> get(KeyT key)
 	{
-		removeExpiredEntries();
+		ifDueRemoveExpiredEntries();
 
 		Optional<CacheEntry> optionalCacheEntry = Optional.ofNullable(cache.get(key));
 		optionalCacheEntry.ifPresent(CacheEntry::markAsUsed);
 
-		return optionalCacheEntry.map(CacheEntry::getElement);
+		return optionalCacheEntry.map(CacheEntry::getValue);
 	}
 
-
 	// EXPIRATION
+	private void ifDueRemoveExpiredEntries()
+	{
+		if(!expireCheckDueTimer.isOver())
+			return;
+
+		expireCheckDueTimer.resetAndStart();
+		removeExpiredEntries();
+	}
+
 	private void removeExpiredEntries()
 	{
 		cache.entrySet().removeIf(e->e.getValue().isExpired());
@@ -61,24 +101,57 @@ public final class LazyKeyCache<KeyT, T>
 
 
 	// ENTRY
+	private abstract class CacheEntry
+	{
+
+		abstract T getValue();
+
+		abstract void markAsUsed();
+
+		abstract boolean isExpired();
+
+	}
+
 	@RequiredArgsConstructor
-	private class CacheEntry
+	private class CacheEntryRespectUsage extends CacheEntry
 	{
 
 		private Instant lastUsage = Instant.now();
-		@Getter private final T element;
+		@Getter private final T value;
 
 
 		// STATUS
-		private void markAsUsed()
+		@Override public void markAsUsed()
 		{
 			lastUsage = Instant.now();
 		}
 
-		private boolean isExpired()
+		@Override public boolean isExpired()
 		{
 			Duration sinceLastUsage = DurationUtil.toNow(lastUsage);
 			return sinceLastUsage.compareTo(expirationDuration) > 0;
+		}
+
+	}
+
+	@RequiredArgsConstructor
+	private class CacheEntryIgnoreUsage extends CacheEntry
+	{
+
+		private final Instant created = Instant.now();
+		@Getter private final T value;
+
+
+		// STATUS
+		@Override public void markAsUsed()
+		{
+
+		}
+
+		@Override public boolean isExpired()
+		{
+			Duration sinceCreation = DurationUtil.toNow(created);
+			return sinceCreation.compareTo(expirationDuration) > 0;
 		}
 
 	}
