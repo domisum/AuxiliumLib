c6cfa7521f8014aa10f7b624f5cf6729edfd2375
domisum <dominik.weissenseel@gmail.com>
2018-08-15 14:19:07 +0200

    Added UnsupportedFileTypeException

---
+++ src/main/java/de/domisum/lib/auxilium/util/java/exceptions/UnsupportedFileTypeException.java
@@ -0,0 +1,25 @@
+package de.domisum.lib.auxilium.util.java.exceptions;
+
+import de.domisum.lib.auxilium.util.java.annotations.API;
+
+@API
+public class UnsupportedFileTypeException extends RuntimeException
+{
+
+	// INIT
+	@API public UnsupportedFileTypeException()
+	{
+
+	}
+
+	@API public UnsupportedFileTypeException(String s)
+	{
+		super(s);
+	}
+
+	@API public UnsupportedFileTypeException(String s, Throwable throwable)
+	{
+		super(s, throwable);
+	}
+
+}
