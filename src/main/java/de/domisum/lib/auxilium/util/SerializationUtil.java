6f555e1a2e8e3057864b94f107c392607fe86a35
domisum <dominik.weissenseel@gmail.com>
2019-06-19 16:44:56 +0200

    Removed SerializationUtil dependency on Apache Commons

--- src/main/java/de/domisum/lib/auxilium/util/SerializationUtil.java
+++ src/main/java/de/domisum/lib/auxilium/util/SerializationUtil.java
@@ -3,9 +3,13 @@ package de.domisum.lib.auxilium.util;
 import de.domisum.lib.auxilium.util.java.annotations.API;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.apache.commons.lang3.SerializationUtils;
+import org.apache.commons.lang3.SerializationException;
 
-import java.io.Serializable;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 @API
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -13,14 +17,35 @@ public final class SerializationUtil
 {
 
 	// SERIALIZATION
-	@API public static <T extends Serializable> byte[] serialize(T object)
+	@API
+	public static <T> byte[] serialize(T object)
 	{
-		return SerializationUtils.serialize(object);
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+		try(ObjectOutputStream out = new ObjectOutputStream(byteArrayOutputStream))
+		{
+			out.writeObject(object);
+		}
+		catch(IOException var11)
+		{
+			throw new SerializationException(var11);
+		}
+
+		return byteArrayOutputStream.toByteArray();
 	}
 
-	@API public static <T extends Serializable> T deserialize(byte[] serialized)
+	@API
+	public static <T> T deserialize(byte[] serialized)
 	{
-		return SerializationUtils.deserialize(serialized);
+		try(ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(serialized)))
+		{
+			// noinspection unchecked
+			return (T) in.readObject();
+		}
+		catch(ClassNotFoundException|IOException var13)
+		{
+			throw new SerializationException(var13);
+		}
 	}
 
 }
