f4e0e32e51bb9a577adba7e3ac656bee13fba1d2
domisum <dominik.weissenseel@gmail.com>
2019-06-23 14:40:47 +0200

    Fixed IoConsumer by adding throws IOException

--- src/main/java/de/domisum/lib/auxilium/contracts/IoConsumer.java
+++ src/main/java/de/domisum/lib/auxilium/contracts/IoConsumer.java
@@ -2,10 +2,12 @@ package de.domisum.lib.auxilium.contracts;
 
 import de.domisum.lib.auxilium.util.java.annotations.API;
 
+import java.io.IOException;
+
 public interface IoConsumer<T>
 {
 
 	@API
-	void accept(T input);
+	void accept(T input) throws IOException;
 
 }
