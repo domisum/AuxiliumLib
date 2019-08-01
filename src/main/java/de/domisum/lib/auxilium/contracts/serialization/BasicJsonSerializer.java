ee2500c5fa10d4937d8c4f0f182d542efc9b1002
domisum <dominik.weissenseel@gmail.com>
2019-08-01 23:31:17 +0200

    Added error message to basic json serializer

--- src/main/java/de/domisum/lib/auxilium/contracts/serialization/BasicJsonSerializer.java
+++ src/main/java/de/domisum/lib/auxilium/contracts/serialization/BasicJsonSerializer.java
@@ -1,27 +1,43 @@
 package de.domisum.lib.auxilium.contracts.serialization;
 
+import com.google.gson.JsonSyntaxException;
 import de.domisum.lib.auxilium.util.java.annotations.API;
 import de.domisum.lib.auxilium.util.json.GsonUtil;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @API
 @RequiredArgsConstructor
 public class BasicJsonSerializer<T> implements JsonSerializer<T>
 {
 
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+
 	// ATTRIBUTES
 	private final Class<T> classToSerialize;
 
 
 	// SERIALIZE
-	@Override public String serialize(T object)
+	@Override
+	public String serialize(T object)
 	{
 		return GsonUtil.get().toJson(object);
 	}
 
-	@Override public T deserialize(String projectString)
+	@Override
+	public T deserialize(String objectString)
 	{
-		return GsonUtil.get().fromJson(projectString, classToSerialize);
+		try
+		{
+			return GsonUtil.get().fromJson(objectString, classToSerialize);
+		}
+		catch(JsonSyntaxException e)
+		{
+			logger.error("failed to deserialize {}", objectString, e);
+			throw e;
+		}
 	}
 
 }
