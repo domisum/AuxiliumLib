6edaa73a478cec1f4d315266c5a78ee888637420
domisum <dominik.weissenseel@gmail.com>
2020-02-20 18:14:14 +0100

    Added ToFromFileSerializer

--- src/main/java/de/domisum/lib/auxilium/contracts/serialization/ToFromFileSerializer.java
+++ src/main/java/de/domisum/lib/auxilium/contracts/serialization/ToFromFileSerializer.java
@@ -6,11 +6,11 @@ import java.io.File;
 import java.io.IOException;
 
 @API
-public interface FileSerializer<T>
+public interface ToFromFileSerializer<T>
 {
 
-	File serialize(T object) throws IOException;
+	File serializeToFile(T object) throws IOException;
 
-	T deserialize(File file) throws IOException;
+	T deserializeFromFile(File file) throws IOException;
 
 }
