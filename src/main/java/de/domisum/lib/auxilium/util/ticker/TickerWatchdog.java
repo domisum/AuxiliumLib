9259fc3f6363ad4e1a3aace563876a776c73a68c
domisum <dominik.weissenseel@gmail.com>
2020-03-10 08:20:01 +0100

    Fixed ticker watchdog concurrent modification exception

--- src/main/java/de/domisum/lib/auxilium/util/ticker/TickerWatchdog.java
+++ src/main/java/de/domisum/lib/auxilium/util/ticker/TickerWatchdog.java
@@ -1,5 +1,6 @@
 package de.domisum.lib.auxilium.util.ticker;
 
+import com.google.common.collect.Sets;
 import de.domisum.lib.auxilium.util.java.ThreadUtil;
 import de.domisum.lib.auxilium.util.ticker.Ticker.Ticking;
 import de.domisum.lib.auxilium.util.ticker.Ticker.TickingStatus;
@@ -7,7 +8,6 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import java.time.Duration;
-import java.util.HashSet;
 import java.util.Set;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -18,12 +18,12 @@ public final class TickerWatchdog
 	private static final Duration INTERVAL = Duration.ofSeconds(1);
 
 	// STATUS
-	private static final Set<Ticking> watchedTickings = new HashSet<>();
+	private static final Set<Ticking> watchedTickings = Sets.newConcurrentHashSet();
 	private static Thread thread = null;
 
 
 	// CONTROL
-	public static synchronized void watch(Ticking ticking)
+	public static void watch(Ticking ticking)
 	{
 		watchedTickings.add(ticking);
 		ensureThreadRunning();
@@ -47,18 +47,12 @@ public final class TickerWatchdog
 		}
 	}
 
-	private static synchronized void tick()
+	private static void tick()
 	{
 		watchedTickings.removeIf(t->t.getStatus() == TickingStatus.DEAD);
 
 		for(var ticking : watchedTickings)
 			ticking.watchdogTick();
-
-		if(watchedTickings.isEmpty())
-		{
-			thread.interrupt();
-			thread = null;
-		}
 	}
 
 }
