867558d7262800a9a4ac0c7ed54e9dc7de51b704
domisum <dominik.weissenseel@gmail.com>
2020-03-29 04:42:58 +0200

    Added more validation methods to ValidationUtil

--- src/main/java/io/domisum/lib/auxiliumlib/util/ValidationUtil.java
+++ src/main/java/io/domisum/lib/auxiliumlib/util/ValidationUtil.java
@@ -23,7 +23,7 @@ public final class ValidationUtil
 	
 	// STRING
 	@API
-	public static void notEmpty(String string, String variableName)
+	public static void notBlank(String string, String variableName)
 	{
 		notNull(string, variableName);
 		
@@ -36,7 +36,7 @@ public final class ValidationUtil
 	@API
 	public static void greaterZero(double number, String variableName)
 	{
-		if(number<=0)
+		if(number <= 0)
 			throw new IllegalArgumentException(variableName+" has to be greater than zero, but was "+number);
 	}
 	
@@ -45,14 +45,68 @@ public final class ValidationUtil
 	@API
 	public static void greaterThan(Duration a, Duration b, String aName, String bName)
 	{
-		if(b.compareTo(a)>0)
+		if(b.compareTo(a) > 0)
 			throw new IllegalArgumentException(
 					aName+" ("+DurationDisplay.display(a)+") has to be greater than "+bName+" ("+DurationDisplay.display(b)
 							+"), but wasn't");
 	}
 	
 	
-	// INTERVAL
+	// SPECIAL VALUES
+	@API
+	public static void validatePortInRange(int port, String portName)
+	{
+		final int MAX_PORT_VALUE = 65535;
+		inIntervalInclIncl(1, MAX_PORT_VALUE, port, portName);
+	}
+	
+	
+	// INTERVAL INT
+	@API
+	public static void inIntervalInclIncl(int start, int end, int value, String variableName)
+	{
+		inInterval(start, true, end, true, value, variableName);
+	}
+	
+	@API
+	public static void inIntervalExclIncl(int start, int end, int value, String variableName)
+	{
+		inInterval(start, false, end, true, value, variableName);
+	}
+	
+	@API
+	public static void inIntervalExclExcl(int start, int end, int value, String variableName)
+	{
+		inInterval(start, false, end, false, value, variableName);
+	}
+	
+	private static void inInterval(int start, boolean startInclusive, int end, boolean endInclusive, int value, String variableName)
+	{
+		if(end < start)
+			throw new IllegalArgumentException("start and end are the wrong way around");
+		
+		if((value < start) || (value > end))
+			throw new IllegalArgumentException(variableName+" has to be in interval "+
+					displayInterval(start, startInclusive, end, endInclusive)+", but was "+value);
+		
+		if((value == start) && !startInclusive)
+			throw new IllegalArgumentException(variableName+" has to be in interval "+
+					displayInterval(start, startInclusive, end, endInclusive)+", but was "+value+" (is equal to start and therefore excluded)");
+		
+		if((value == end) && !endInclusive)
+			throw new IllegalArgumentException(variableName+" has to be in interval "+
+					displayInterval(start, startInclusive, end, endInclusive)+", but was "+value+" (is equal to end and therefore excluded)");
+	}
+	
+	private static String displayInterval(int start, boolean startInclusive, int end, boolean endInclusive)
+	{
+		String startBracket = startInclusive ? "[" : "]";
+		String endBracket = endInclusive ? "]" : "[";
+		return startBracket+start+" to "+end+endBracket;
+	}
+	
+	
+	// INTERVAL DOUBLE
 	@API
 	public static void inIntervalInclIncl(double start, double end, double value, String variableName)
 	{
@@ -73,10 +127,10 @@ public final class ValidationUtil
 	
 	private static void inInterval(double start, boolean startInclusive, double end, boolean endInclusive, double value, String variableName)
 	{
-		if(end<start)
+		if(end < start)
 			throw new IllegalArgumentException("start and end are the wrong way around");
 		
-		if((value<start) || (value>end))
+		if((value < start) || (value > end))
 			throw new IllegalArgumentException(variableName+" has to be in interval "+
 					displayInterval(start, startInclusive, end, endInclusive)+", but was "+value);
 		
@@ -91,12 +145,8 @@ public final class ValidationUtil
 	
 	private static String displayInterval(double start, boolean startInclusive, double end, boolean endInclusive)
 	{
-		String startBracket = startInclusive ?
-				"[" :
-				"]";
-		String endBracket = endInclusive ?
-				"]" :
-				"[";
+		String startBracket = startInclusive ? "[" : "]";
+		String endBracket = endInclusive ? "]" : "[";
 		return startBracket+start+" to "+end+endBracket;
 	}
 	
