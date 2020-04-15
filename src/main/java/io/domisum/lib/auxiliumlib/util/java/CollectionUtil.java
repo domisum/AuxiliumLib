2168b566d61cb2d273710069f8d2c2212a11c69e
domisum <dominik.weissenseel@gmail.com>
2020-04-15 16:22:51 +0200

    Added CollectionUtil

---
+++ src/main/java/io/domisum/lib/auxiliumlib/util/java/CollectionUtil.java
@@ -0,0 +1,25 @@
+package io.domisum.lib.auxiliumlib.util.java;
+
+import io.domisum.lib.auxiliumlib.annotations.API;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@API
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CollectionUtil
+{
+	
+	@API
+	public static <T extends Comparable<T>> List<T> sorted(Collection<T> collection)
+	{
+		var list = new ArrayList<>(collection);
+		Collections.sort(list);
+		return list;
+	}
+	
+}
