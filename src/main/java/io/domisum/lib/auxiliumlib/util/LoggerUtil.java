2e88d16aca1108aa82916320b8b90260c441bd7e
domisum <dominik.weissenseel@gmail.com>
2020-04-17 13:15:38 +0200

    Removed weak warning logging

--- src/main/java/io/domisum/lib/auxiliumlib/util/LoggerUtil.java
+++ src/main/java/io/domisum/lib/auxiliumlib/util/LoggerUtil.java
@@ -5,8 +5,7 @@ import io.domisum.lib.auxiliumlib.exceptions.IncompleteCodeError;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
-
-import java.util.Optional;
+import org.slf4j.event.Level;
 
 @API
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -23,40 +22,12 @@ public final class LoggerUtil
 			logger.debug(message, objects);
 		else if(level == Level.INFO)
 			logger.info(message, objects);
-		else if(level == Level.WARNING)
+		else if(level == Level.WARN)
 			logger.warn(message, objects);
 		else if(level == Level.ERROR)
 			logger.error(message, objects);
 		else
 			throw new IncompleteCodeError("Unknown log level: "+level);
-	}
-	
-	@API
-	public static void logWeakWarning(Logger logger, boolean isTooWeak, String message, Object... objects)
-	{
-		var level = isTooWeak ? Level.INFO : Level.WARNING;
-		log(logger, level, message, objects);
-	}
-	
-	@API
-	public static void logWeakWarning(Logger logger, Optional<Boolean> isTooWeakOptional, String message, Object... objects)
-	{
-		boolean isTooWeak = isTooWeakOptional.orElse(false);
-		logWeakWarning(logger, isTooWeak, message, objects);
-	}
-	
-	
-	// LEVEL
-	@API
-	public enum Level
-	{
-		
-		TRACE,
-		DEBUG,
-		INFO,
-		WARNING,
-		ERROR
-		
 	}
 	
 }
