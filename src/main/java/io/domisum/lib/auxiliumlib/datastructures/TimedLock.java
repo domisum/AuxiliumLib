46ba1d2d819869a74c6c9e939941f61f090dd6e4
domisum <dominik.weissenseel@gmail.com>
2020-06-30 17:08:50 +0200

    Added TimedLock

---
+++ src/main/java/io/domisum/lib/auxiliumlib/datastructures/TimedLock.java
@@ -0,0 +1,83 @@
+package io.domisum.lib.auxiliumlib.datastructures;
+
+import io.domisum.lib.auxiliumlib.annotations.API;
+import io.domisum.lib.auxiliumlib.util.TimeUtil;
+import lombok.RequiredArgsConstructor;
+
+import javax.annotation.Nullable;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class TimedLock<T>
+{
+	
+	// SETTINGS
+	@Nullable
+	private final Duration defaultDuration;
+	
+	// STATUS
+	private final Map<T,Instant> lockedUntilMap = new HashMap<>();
+	
+	
+	// LOCK
+	@API
+	public synchronized void lock(T key, Duration duration)
+	{
+		lockedUntilMap.put(key, Instant.now().plus(duration));
+	}
+	
+	@API
+	public synchronized void lock(T key)
+	{
+		if(defaultDuration == null)
+			throw new IllegalStateException("Can't use this method when no defaultDuration was given in constructor");
+		
+		lock(key, defaultDuration);
+	}
+	
+	@API
+	public synchronized void unlock(T key)
+	{
+		lockedUntilMap.remove(key);
+	}
+	
+	
+	// STATUS
+	@API
+	public synchronized boolean isLocked(T key)
+	{
+		var lockedUntil = lockedUntilMap.get(key);
+		if(lockedUntil == null)
+			return false;
+		
+		if(TimeUtil.isInFuture(lockedUntil))
+			return true;
+		else
+		{
+			lockedUntilMap.remove(key);
+			return false;
+		}
+	}
+	
+	@API
+	public synchronized Optional<Duration> getRemainingLockDuration(T key)
+	{
+		var lockedUntil = lockedUntilMap.get(key);
+		if(lockedUntil == null)
+			return Optional.empty();
+		
+		var remainingLockDuration = TimeUtil.until(lockedUntil);
+		if(remainingLockDuration.isZero() || remainingLockDuration.isNegative())
+		{
+			lockedUntilMap.remove(key);
+			return Optional.empty();
+		}
+		
+		return Optional.of(remainingLockDuration);
+	}
+	
+}
