0f0783b86c2830021381ac2b7c4494e54732aa29
domisum <me@domisum.io>
2020-08-19 19:59:36 +0200

    Added UncheckedSqlException

---
+++ src/main/java/io/domisum/lib/auxiliumlib/exceptions/UncheckedSqlException.java
@@ -0,0 +1,24 @@
+package io.domisum.lib.auxiliumlib.exceptions;
+
+import io.domisum.lib.auxiliumlib.annotations.API;
+
+import java.sql.SQLException;
+
+@API
+public class UncheckedSqlException
+		extends RuntimeException
+{
+	
+	@API
+	public UncheckedSqlException(String message, SQLException cause)
+	{
+		super(message, cause);
+	}
+	
+	@API
+	public UncheckedSqlException(SQLException cause)
+	{
+		super(cause);
+	}
+	
+}
