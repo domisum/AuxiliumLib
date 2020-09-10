5011ef52fc43f373493fb03842488237cbf06d5a
domisum <me@domisum.io>
2020-09-10 16:22:28 +0200

    Add progress logging to MultithreaderQueue_Collection

--- src/main/java/io/domisum/lib/auxiliumlib/thread/multithreader/queues/MultithreaderQueue_Collection.java
+++ src/main/java/io/domisum/lib/auxiliumlib/thread/multithreader/queues/MultithreaderQueue_Collection.java
@@ -2,7 +2,13 @@ package io.domisum.lib.auxiliumlib.thread.multithreader.queues;
 
 import io.domisum.lib.auxiliumlib.annotations.API;
 import io.domisum.lib.auxiliumlib.thread.multithreader.MultithreaderQueue;
+import io.domisum.lib.auxiliumlib.util.TimeUtil;
+import io.domisum.lib.auxiliumlib.util.math.MathUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.Queue;
@@ -13,15 +19,41 @@ public class MultithreaderQueue_Collection<T>
 	implements MultithreaderQueue<T>
 {
 	
-	// STATE
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+	
+	// CONSTANTS
+	private static final Duration LOG_PROGRESS_INTERVAL = Duration.ofSeconds(5);
+	
+	// SETTINGS
+	private final boolean logProgress;
+	
+	// DATA
 	private final Queue<T> queue = new ConcurrentLinkedQueue<>();
+	private final int totalElementsCount;
+	
+	// STATUS
+	private Instant lastProgressLogInstant = Instant.MIN;
 	
 	
 	// INIT
 	@API
-	public MultithreaderQueue_Collection(Collection<T> collection)
+	public static <T> MultithreaderQueue_Collection<T> create(Collection<T> collection)
 	{
+		return new MultithreaderQueue_Collection<>(false, collection);
+	}
+	
+	@API
+	public static <T> MultithreaderQueue_Collection<T> createWithLogProgress(Collection<T> collection)
+	{
+		return new MultithreaderQueue_Collection<>(false, collection);
+	}
+	
+	protected MultithreaderQueue_Collection(boolean logProgress, Collection<T> collection)
+	{
+		this.logProgress = logProgress;
 		queue.addAll(collection);
+		totalElementsCount = collection.size();
 	}
 	
 	
@@ -35,6 +67,17 @@ public class MultithreaderQueue_Collection<T>
 	@Override
 	public Optional<T> poll()
 	{
+		if(logProgress && TimeUtil.isOlderThan(lastProgressLogInstant, LOG_PROGRESS_INTERVAL))
+		{
+			int completedItems = totalElementsCount-queue.size();
+			double fractionCompleted = completedItems/(double) totalElementsCount;
+			double completedPercentage = fractionCompleted*100;
+			String progressDisplay = MathUtil.round(completedPercentage, 1)+"%";
+			
+			logger.info("Multithreader progress: {}", progressDisplay);
+			lastProgressLogInstant = Instant.now();
+		}
+		
 		var calcNullable = queue.poll();
 		return Optional.ofNullable(calcNullable);
 	}
