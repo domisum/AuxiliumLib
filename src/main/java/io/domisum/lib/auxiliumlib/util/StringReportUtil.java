8de19db31a33af9ecae84cdbbaa308a965bd25d0
domisum <me@domisum.io>
2021-01-26 09:34:44 +0100

    Add StringReportUtil.ioMapReport

--- src/main/java/io/domisum/lib/auxiliumlib/util/StringReportUtil.java
+++ src/main/java/io/domisum/lib/auxiliumlib/util/StringReportUtil.java
@@ -2,9 +2,12 @@ package io.domisum.lib.auxiliumlib.util;
 
 import io.domisum.lib.auxiliumlib.PHR;
 import io.domisum.lib.auxiliumlib.annotations.API;
+import io.domisum.lib.auxiliumlib.contracts.IoFunction;
+import io.domisum.lib.auxiliumlib.exceptions.ProgrammingError;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -18,14 +21,37 @@ import java.util.function.Function;
 public final class StringReportUtil
 {
 	
+	private static final String QUOT = "'";
+	
+	
 	@API
-	public static String report(List<?> list)
+	public static <T> String ioMapReport(List<T> list, IoFunction<T, Object> valueFunction)
+		throws IOException
 	{
 		var itemDisplays = new ArrayList<String>();
 		for(var item : list)
-			itemDisplays.add(PHR.r(" - {}", item.toString()));
+			itemDisplays.add(PHR.r(" - {}", valueFunction.apply(item)));
 		
 		return StringListUtil.list(itemDisplays, "\n");
+	}
+	
+	@API
+	public static <T> String report(List<T> list, Function<T, Object> valueFunction)
+	{
+		try
+		{
+			return ioMapReport(list, item->valueFunction.apply(item));
+		}
+		catch(IOException e)
+		{
+			throw new ProgrammingError("Should never happen", e);
+		}
+	}
+	
+	@API
+	public static <T> String report(List<T> list)
+	{
+		return report(list, Objects::toString);
 	}
 	
 	@API
@@ -34,19 +60,17 @@ public final class StringReportUtil
 		return report(new ArrayList<>(collection));
 	}
 	
+	
 	@API
 	public static String report(Map<?, ?> map)
 	{
-		return report(map,
-			o->(o instanceof String) ?
-				"'"+o+"'" :
-				Objects.toString(o));
+		return report(map, Objects::toString);
 	}
 	
 	@API
 	public static <T> String report(Map<?, T> map, Function<T, Object> valueFunction)
 	{
-		return report(map, k->(k instanceof String) ? "'"+k+"'" : k, valueFunction);
+		return report(map, k->(k instanceof String) ? QUOT+k+QUOT : k, valueFunction);
 	}
 	
 	@API
