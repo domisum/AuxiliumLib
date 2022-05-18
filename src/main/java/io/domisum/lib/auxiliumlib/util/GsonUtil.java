8ff605fbff9fabe0987f80cbff8c90dd0c411e25
domisum <me@domisum.io>
2022-05-18 17:45:43 +0200

    Add GsonUtil.addProperty

--- src/main/java/io/domisum/lib/auxiliumlib/util/GsonUtil.java
+++ src/main/java/io/domisum/lib/auxiliumlib/util/GsonUtil.java
@@ -2,9 +2,12 @@ package io.domisum.lib.auxiliumlib.util;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import io.domisum.lib.auxiliumlib.annotations.API;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
+import java.util.function.BiConsumer;
 
 @API
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -26,6 +29,41 @@ public final class GsonUtil
 				.create();
 		
 		return gson;
+	}
+	
+	
+	public static void addProperty(JsonObject base, String path, String value)
+	{
+		addProperty(base, path, (o, k)->o.addProperty(k, value));
+	}
+	
+	public static void addProperty(JsonObject base, String path, Number value)
+	{
+		addProperty(base, path, (o, k)->o.addProperty(k, value));
+	}
+	
+	public static void addProperty(JsonObject base, String path, Boolean value)
+	{
+		addProperty(base, path, (o, k)->o.addProperty(k, value));
+	}
+	
+	public static void addProperty(JsonObject base, String path, BiConsumer<JsonObject, String> addProperty)
+	{
+		int dotIndex = path.indexOf('.');
+		if(dotIndex == -1)
+		{
+			addProperty.accept(base, path);
+			return;
+		}
+		
+		String frontPathPart = path.substring(0, dotIndex);
+		String remainingPathPart = path.substring(dotIndex+1);
+		
+		var container = base.has(frontPathPart) ? base.getAsJsonObject(frontPathPart) : new JsonObject();
+		if(!base.has(frontPathPart))
+			base.add(frontPathPart, container);
+		
+		addProperty(container, remainingPathPart, addProperty);
 	}
 	
 }
