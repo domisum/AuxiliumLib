6384a7fe550db80a0a5d9e0559a36161ba58695e
domisum <me@domisum.io>
2023-04-11 21:22:20 +0200

    Add IoIterator.toList

--- src/main/java/io/domisum/lib/auxiliumlib/contracts/IoIterator.java
+++ src/main/java/io/domisum/lib/auxiliumlib/contracts/IoIterator.java
@@ -1,6 +1,8 @@
 package io.domisum.lib.auxiliumlib.contracts;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public interface IoIterator<T>
 {
@@ -10,5 +12,15 @@ public interface IoIterator<T>
 	
 	T next()
 		throws IOException;
+	
+	
+	default List<T> toList()
+		throws IOException
+	{
+		var items = new ArrayList<T>();
+		while(hasNext())
+			items.add(next());
+		return items;
+	}
 	
 }
