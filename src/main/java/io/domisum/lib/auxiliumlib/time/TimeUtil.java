722bb097bc3f16660738ad54618caf6e8d59f945
domisum <me@domisum.io>
2025-03-05 10:26:03 +0100

    Add TimeUtil.fromDaysDecimal()

--- src/main/java/io/domisum/lib/auxiliumlib/time/TimeUtil.java
+++ src/main/java/io/domisum/lib/auxiliumlib/time/TimeUtil.java
@@ -38,57 +38,63 @@ public final class TimeUtil
 	@API
 	public static double getDaysDecimal(Duration duration)
 	{
-		return duration.getSeconds()/(double) Duration.ofDays(1).getSeconds();
+		return duration.getSeconds() / (double) Duration.ofDays(1).getSeconds();
 	}
 	
 	@API
 	public static double getHoursDecimal(Duration duration)
 	{
-		return duration.getSeconds()/(double) Duration.ofHours(1).getSeconds();
+		return duration.getSeconds() / (double) Duration.ofHours(1).getSeconds();
 	}
 	
 	@API
 	public static double getMinutesDecimal(Duration duration)
 	{
-		return duration.getSeconds()/(double) Duration.ofMinutes(1).getSeconds();
+		return duration.getSeconds() / (double) Duration.ofMinutes(1).getSeconds();
 	}
 	
 	@API
 	public static double getSecondsDecimal(Duration duration)
 	{
-		return duration.toMillis()/(double) Duration.ofSeconds(1).toMillis();
+		return duration.toMillis() / (double) Duration.ofSeconds(1).toMillis();
 	}
 	
 	@API
 	public static double getMillisecondsDecimal(Duration duration)
 	{
-		return duration.getSeconds()*1000+(duration.toNanosPart()/(double) (1000*1000));
+		return duration.getSeconds() * 1000 + (duration.toNanosPart() / (double) (1000 * 1000));
 	}
 	
 	@API
 	public static Duration fromSecondsDecimal(double seconds)
 	{
-		return Duration.ofMillis(Math.round(seconds*1000));
+		return Duration.ofMillis(Math.round(seconds * 1000));
 	}
 	
 	@API
 	public static Duration fromMinutesDecimal(double minutes)
 	{
-		return Duration.ofMillis(Math.round(minutes*60*1000));
+		return Duration.ofMillis(Math.round(minutes * 60 * 1000));
 	}
 	
 	@API
 	public static Duration fromHoursDecimal(double hours)
 	{
-		return Duration.ofSeconds(Math.round(hours*60*60));
+		return Duration.ofSeconds(Math.round(hours * 60 * 60));
+	}
+	
+	@API
+	public static Duration fromDaysDecimal(double days)
+	{
+		return Duration.ofMinutes(Math.round(days * 24 * 60));
 	}
 	
 	
 	@API
 	public static Duration multiply(Duration duration, double factor)
 	{
-		long seconds = Math.round(factor*duration.getSeconds());
-		long nanos = Math.round(factor*duration.getNano());
+		long seconds = Math.round(factor * duration.getSeconds());
+		long nanos = Math.round(factor * duration.getNano());
 		return Duration.ofSeconds(seconds, nanos);
 	}
 	
@@ -99,15 +105,15 @@ public final class TimeUtil
 	{
 		final int secondsInMinute = 60;
 		
-		long totalSeconds = (int) Math.ceil(duration.toMillis()/(double) 1000);
-		long displayMinutes = totalSeconds/secondsInMinute;
-		long displaySeconds = totalSeconds%secondsInMinute;
+		long totalSeconds = (int) Math.ceil(duration.toMillis() / (double) 1000);
+		long displayMinutes = totalSeconds / secondsInMinute;
+		long displaySeconds = totalSeconds % secondsInMinute;
 		
-		String secondsString = displaySeconds+"";
+		String secondsString = displaySeconds + "";
 		if(secondsString.length() == 1)
-			secondsString = "0"+secondsString;
+			secondsString = "0" + secondsString;
 		
-		return displayMinutes+":"+secondsString;
+		return displayMinutes + ":" + secondsString;
 	}
 	
 	
